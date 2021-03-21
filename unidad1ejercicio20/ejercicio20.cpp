dd6200965d7ef82e65f8e210968cfabaca38629d
Alexix87 <alexis.cabo.87@gmail.com>
2021-03-21 12:13:12 -0300

    unidad 1 ejercicio 20

---
+++ unidad1ejercicio20/ejercicio20.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b;
+    
+    cout << "Ingrese a: ";
+    cin >> a;
+    cout << "Ingrese b: ";
+    cin >> b;
+    
+    cout << "El resto de la division entera es: " << a%b << endl;
+    system("pause");
+
+    return 0;
+}
